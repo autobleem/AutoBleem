01dce843c26059af26021dfe9fab247588bdcc34
root <root@ubuntu18.fritz.box>
2018-12-16 21:26:25 +0000

    New build

--- build_arm/payload/lolhack/boot_autobleem.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Update Database on the fly
-cd /media/cbleemsync
-mkdir -p /media/System/
-mkdir -p /media/System/Databases/
-./run.sh
-sync
-cp /media/System/regional.db /media/System/Databases/
-sync
