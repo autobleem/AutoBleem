8e782fa927ecd1c173e660b96b0756226277314a
screemer <jakubowicze@gmail.om>
2018-12-16 01:30:32 +0000

    First Release

--- payload/lolhack/boot_autobleem.sh
+++ payload/lolhack/boot_autobleem.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 # Update Database on the fly
 cd /media/cbleemsync
+mkdir -p /media/System/
+mkdir -p /media/System/Databases/
 ./run.sh
 sync
 cp /media/System/regional.db /media/System/Databases/
