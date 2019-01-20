71580654ab43df82791f327ff4330fe0e3619914
screemer <jakubowicze@gmail.om>
2019-01-20 00:53:19 +0000

    PBP serial scan

--- src/code/starter.cpp
+++ src/code/starter.cpp
@@ -64,8 +64,10 @@ int main (int argc, char *argv[])
     for (const auto& arg : arguments)
         argvNew.push_back((char*)arg.data());
 
-    argvNew.push_back("-cfg");
-    argvNew.push_back("/data/AppData/sony/title/pcsx.cfg");
+    char cfg[] = "-cfg";
+    char cfgpath[] = "/data/AppData/sony/title/pcsx.cfg";
+    argvNew.push_back(cfg);
+    argvNew.push_back(cfgpath);
 
     argvNew.push_back(nullptr);
     execute(argvNew.size() - 1, argvNew.data());
