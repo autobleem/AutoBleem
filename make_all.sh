6abecaccc33b135eb1d8976f45e5bf4d13882d25
AJ <screemer@li465-118.members.linode.com>
2019-01-25 23:23:59 +0000

    0.5 alpha test

--- make_all.sh
+++ make_all.sh
@@ -2,14 +2,14 @@ git pull
 rm -rf ./build
 rm -rf ./build_arm
 
-mkdir -p build
+# mkdir -p build
 mkdir -p build_arm
 
-cd ./build
-cmake ..
-make
+#cd ./build
+#cmake ..
+#make
 
-cd ..
+#cd ..
 cd ./build_arm
 cmake -DCMAKE_TOOLCHAIN_FILE=../PS1Ctoolchain.cmake ..
 make
