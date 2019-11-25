86562bbbfe058c651c41a52241a738667fd3e392
Axanar <steve@iterator.com>
2019-11-24 21:58:04 -0500

    fix the line endings.  the sh scripts in payload/rc in git had windows line endins.  the sh scripts in the 0.7.1 release had linux line endings. alsoretroarch.sh script in payload/rc did not match what was in 0.7.1

--- payload/Autobleem/rc/retroarch.sh
+++ payload/Autobleem/rc/retroarch.sh
@@ -1,86 +1,13 @@
-#
-# RetroBoot 0.5.5 based code as agreed with genderbent
-#
+#!/bin/sh
 
-#!/bin/bash
 #kill sony stuffs and set powermanagement parameters
 killall -s KILL showLogo sonyapp ui_menu auto_dimmer pcsx dimmer
 echo 2 > /data/power/disable
 sync
 
-#duplicate internal PSX bios to a bunch of bios files required by retroarch
-if [ ! -f "/media/RetroArch/system/scph102b.bin" ]
-    then
-    cp -f "/gaadata/system/bios/romw.bin" "/media/RetroArch/system/scph102b.bin"
-    fi
-if [ ! -f "/media/RetroArch/system/scph5500.bin" ]
-    then
-    cp -f "/gaadata/system/bios/romw.bin" "/media/RetroArch/system/scph5500.bin"
-    fi
-if [ ! -f "/media/RetroArch/system/scph5501.bin" ]
-    then
-    cp -f "/gaadata/system/bios/romw.bin" "/media/RetroArch/system/scph5501.bin"
-    fi
-if [ ! -f "/media/RetroArch/system/scph5502.bin" ]
-    then
-    cp -f "/gaadata/system/bios/romw.bin" "/media/RetroArch/system/scph5502.bin"
-    fi
-
-#make default retroarch config active, if its not already active
-if [ ! -f "/media/RetroArch/.config/retroarch/retroarch.cfg" ]
-    then
-    cp -f "/media/RetroArch/.config/retroarch/retroarch_DEFAULT.cfg" "/media/RetroArch/.config/retroarch/retroarch.cfg"
-    fi
-sync
-
-#create RA cache folder
-mkdir -p "/tmp/ra_cache"
-
-# Start RA log monitor 
-sh /media/Autobleem/rc/rb_monitor.sh &
-MONPID=$!
-
-# Start RetroArch.  Restart it if it crashes.
-export XDG_CONFIG_HOME=/media/
-while : ; do
-    rm /tmp/retroboot/.monitor_killed_ra
-   # /media/retroarch/retroarch --config /media/retroarch/config/retroarch.cfg --menu &> /media/retroarch/retroarch.log
-    HOME=/media/RetroArch /media/RetroArch/retroarch -v --menu --config /media/RetroArch/.config/retroarch/retroarch.cfg &> /media/RetroArch/retroarch.log
-    LVL=$?
-
-    if [ $LVL -eq 0 ] && [ ! -f /tmp/retroboot/.monitor_killed_ra ]; then
-        break
-    fi
-
-    echo 0 > /sys/class/leds/green/brightness
-    echo 1 > /sys/class/leds/red/brightness
-
-    mv /media/RetroArch/retroarch.log "/media/RetroArch/retroarch_crash.log"
-    printf "\n--End of retroarch.log--\n\nOutput from dmesg:\n\n" >> /media/RetroArch/retroarch_crash.log
-    dmesg >> /media/RetroArch/retroarch_crash.log
-    printf "\n--End of Log--\n" >> /media/RetroArch/retroarch_crash.log
-
-
-
-    # Flash leds to indicate restart
-    for i in 1 2 3 4
-    do
-        echo 1 > /sys/class/leds/red/brightness
-        usleep 125000
-        echo 0 > /sys/class/leds/red/brightness
-        usleep 125000
-    done
-
-    echo 1 > /sys/class/leds/green/brightness
-done
-
-# Kill the monitor
-kill $MONPID
-
-
-rm -rf "/tmp/ra_cache"
-sync
+sh /media/retroarch/retroboot/bin/launch_rfa.sh
 
 #return to Autobleem UI
 cd /media/Autobleem/
+rm /tmp/.abload 
 ./start.sh
