99dcfede3edb7391f020b37e9c2cd6fee93ac395
screemerpl <screemer1@o2.pl>
2019-12-09 05:59:28 +0000

    Adding binaries for Apps

--- payload/Apps/amiberry/run.sh
+++ payload/Apps/amiberry/run.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 
-export HOME=/media/Apps/amiberry
+HOME=/media/Apps/amiberry
 cd /media/Apps/amiberry
 cp -f conf/default/* conf/
-./amiberry  -config=default -no-gui
-
-
-
+./amiberry  -config=./conf/AB-A500.uae 
 
