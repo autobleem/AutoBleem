2dacc702d35e521f00ada25602a43d5bcaae062c
Axanar <steve@iterator.com>
2020-02-02 17:46:36 -0500

    [feature/initNetworkOnBoot]: add option to auto init the network on boot

--- src/code/gui/menus/gui_networkMenu.h
+++ src/code/gui/menus/gui_networkMenu.h
@@ -18,20 +18,25 @@ public:
 
     void readCfgFile();
     void writeCfgFile();
-    void initializeWifi();
+    static void initializeWifi();   // we currently can't tell if it was successful or not
 
     virtual std::string getTitle() override { return "-=" + _("-=" + _("Edit Network SSID and Password (ssid.cfg)") + "=-") + "=-"; }
     virtual std::string getStatusLine() override;   // returns the status line at the bottom
 
     std::string ssid, password;
-    std::string cfgPath, runPath;
+
+    static std::string getCfgPath() { return Env::getPathToBleemsyncCFGDir() + sep + "ssid.cfg"; }
+    static std::string getRunPath() { return Env::getPathToApps() + sep + "bleemsync_networking" + sep + "run.sh"; }
+    static std::string getWpaSupplicantPath()
+        { return Env::getPathToBleemsyncDir() + sep + "network/etc/wpa_supplicant/wpa_supplicant.conf"; }
+
+    static std::string getSSID();
+    static std::string getSSIDfrom_ssidcfg();
+    static std::string getSSIDfrom_wpa_supplicant();
 
     void doCircle_Pressed() override;
-//    void doSquare_Pressed() override;
-//    void doTriangle_Pressed() override;
     void doCross_Pressed() override;
 
     void doEnter() { doCross_Pressed(); }
     void doEscape() { doCircle_Pressed(); }
-//    void doDelete() { doSquare_Pressed(); }
 };
