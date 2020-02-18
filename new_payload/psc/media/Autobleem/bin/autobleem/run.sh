db53d55977e4c002b68513afc3da9db8c98bdc41
Swingflip <ryan@ryanhamlin.co.uk>
2020-02-18 12:10:32 +0000

    Update run.sh

--- new_payload/psc/media/Autobleem/bin/autobleem/run.sh
+++ new_payload/psc/media/Autobleem/bin/autobleem/run.sh
@@ -4,5 +4,5 @@ AUTOBLEEM_PATH="/media/Autobleem"
 
 mkdir -p "${AUTOBLEEM_PATH}/Games" "${AUTOBLEEM_PATH}/roms" "${AUTOBLEEM_PATH}/System/Databases" "${AUTOBLEEM_PATH}/System/Logs"
 
-LD_LIBRARY_PATH="/tmp/lib" ./autobleem-gui "${AUTOBLEEM_PATH}/System/Databases/regional.db" "${AUTOBLEEM_PATH}/Games/" > "/media//Autobleem/System/Logs/AB_out.txt" 2> "${AUTOBLEEM_PATH}/System/Logs/AB_err.txt"
+LD_LIBRARY_PATH="/tmp/lib" ./autobleem-gui "/media" > "/media/Autobleem/System/Logs/AB_out.txt" 2> "${AUTOBLEEM_PATH}/System/Logs/AB_err.txt"
 
